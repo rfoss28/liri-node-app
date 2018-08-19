2ef573cfe4b47fe5892cdfc15bd6503a81fc0578
Ryan <rfoss28@comcast.net>
2018-08-19 10:41:17 -0400

    copied files to code folder

---
+++ liri.js
@@ -0,0 +1,18 @@
+//code to read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+// imports the keys.js file
+var keys = require("./keys.js");
+
+this
+var spotify = new Spotify(keys.spotify);
+
+// Make it so liri.js can take in one of the following commands:
+
+// concert-thi
+
+// spotify-this-song
+// movie-thisgfgfdggf
+
+
+// do-what-it-saysdf

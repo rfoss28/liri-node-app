981e82d6cc3dc2c82f608f10dc6b397069b4b17c
Ryan <rfoss28@comcast.net>
2018-08-22 08:54:44 -0400

    commit

--- liri.js
+++ liri.js
@@ -4,12 +4,13 @@ require("dotenv").config();
 // imports the keys.js file
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
-var Bit = require("bandsintown");
 var fs = require("fs");
 var request = require("request");
 
 var spotify = new Spotify(keys.spotify);
 // var concert = new Bit(keys.bit);
+
+// var bit= new Bit (keys.bandsintown);
 
 var input = process.argv;
 var command = input[2];
@@ -18,22 +19,25 @@ var command = input[2];
 var name = "";
 
 for (var i = 3; i < input.length; i++) {
-  if (i > 2 && i < input.length) {
+  if (i > 3 && i < input.length) {
     name = name + "+" + input[i];
   } else {
     name += input[i];
   }
 }
 
-// Make it so liri.js can take in one of the following commands:
 
+
+decisionSwitch();
+// Make it so liri.js can take in one of the following commands:
+function decisionSwitch(){
 switch (command) {
   case "concert-this":
-    callBit();
+    callBit(name);
     break;
 
   case "spotify-this-song":
-    callSpotify();
+    callSpotify(name);
     break;
 
   case "movie-this":
@@ -47,30 +51,66 @@ switch (command) {
   default:
     console.log("Invalid input, try again.");
 }
+}
 
-function callBit() {}
+function callBit() {
 
-function callSpotify() {}
 
-function callOmdb(movieName) {
- ;
+    var queryUrl ="https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
+console.log(queryUrl);
+  request(queryUrl, function(error, response, body) {
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.stringify(body).venue);
+    }
+});
+}
+function callSpotify() {
+if( name===""){
+  name="The Sign Ace of Base"
+}
+
+
+ spotify.search({ type: 'track', query: name , limit: '10'}, function(error, data) {
+  if (error) {
+    return console.log('Error occurred: ' + error);
+  }
+
+ var song= data.tracks.items[0];
+console.log("Artist(s) - " + song.artists[0].name);
+console.log("Name of song - " + song.name);
+console.log("Preview link on spotify - " + song.preview_url);
+console.log("Album - " + song.album.name);
+
+});
+
+  //Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+
+
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+}
+
+function callOmdb() {
+
 
   // This line is just to help us debug against the actual URL.
   console.log(queryUrl);
   console.log(name);
   if (name===""){
     name= "Mr. Nobody";
-    console.log(name);
+    console.log(JSON.parse(body).name);
   }
 
-  var queryUrl =
-  "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy"
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&tomatoes=true&apikey=trilogy";
 
   request(queryUrl, function(error, response, body) {
     // If the request is successful
     if (!error && response.statusCode === 200) {
       // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
       console.log("Title - " + JSON.parse(body).Title);
       console.log("Release Year: - " + JSON.parse(body).Year);
       console.log("IMDB Rating - " + JSON.parse(body).imdbRating);
@@ -83,4 +123,16 @@ function callOmdb(movieName) {
   });
 }
 
-function doWhatItSays() {}
+function doWhatItSays() {
+
+  fs.readFile("./random.txt", "utf8", function(error, fileText){
+		if (error) throw error;
+
+    fileText = fileText.split(",");
+    command=fileText[0];
+    console.log(command);
+    name=fileText[1];
+    console.log(name);
+    decisionSwitch();
+});
+}
